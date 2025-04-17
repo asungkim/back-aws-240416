1fd92c3946273517d359686365f40d2b7614edb3
asungkim <asungkim007@naver.com>
2025-04-17 09:23:12 +0900

    aws bucket 추가

---
+++ src/main/kotlin/org/example/back2aws/HomeController.kt
@@ -0,0 +1,22 @@
+package org.example.back2aws
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import software.amazon.awssdk.services.s3.S3Client
+
+@RestController
+class HomeController(
+    private val s3Client: S3Client
+) {
+
+    @GetMapping
+    fun home(): String {
+        return "Hi"
+    }
+
+
+    @GetMapping("/buckets")
+    fun buckets(): List<String> {
+        return s3Client.listBuckets().buckets().map { it.name() }
+    }
+}
