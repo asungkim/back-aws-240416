0472a92e622ba5b8d6574345445712a33c4560ea
asungkim <asungkim007@naver.com>
2025-04-17 09:17:16 +0900

    git init

---
+++ src/main/kotlin/org/example/back2aws/Back2AwsApplication.kt
@@ -0,0 +1,11 @@
+package org.example.back2aws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Back2AwsApplication
+
+fun main(args: Array<String>) {
+    runApplication<Back2AwsApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/back2aws/Back2AwsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.back2aws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Back2AwsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
