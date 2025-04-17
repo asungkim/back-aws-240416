ff840719683f9846a5d9581df43d500e9680b286
asungkim <asungkim007@naver.com>
2025-04-17 10:38:31 +0900

    github actions

--- src/main/kotlin/org/example/back2aws/HomeController.kt
+++ src/main/kotlin/org/example/back2aws/HomeController.kt
@@ -1,5 +1,6 @@
 package org.example.back2aws
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.s3.S3Client
@@ -9,9 +10,12 @@ class HomeController(
     private val s3Service: S3Service
 ) {
 
+    @Value("\${custom.secretWord}")
+    private val secretWord: String = ""
+
     @GetMapping
     fun home(): String {
-        return "Hi"
+        return "Hi, $secretWord"
     }
 
 
